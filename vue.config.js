d5708621a9a63bea6a1b2951cf20654c5eeb1c83
RianKasim21 <riankasim421@gmail.com>
2024-10-16 20:43:48 +0700

    test

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/bansos/' // Ganti dengan nama repository Anda
+    ? '/soal.github.io/' // Ganti dengan nama repository Anda
     : '/'
 })
